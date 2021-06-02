68e53805ff2935e2de6ecb6768f08ce5b00ba649
Y.T <82093403+Y-T-2525@users.noreply.github.com>
2021-06-02 22:17:11 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $(function() {
             headrOpacity.css('opacity','0.7');
         }
 
-        if ($(this).scrollTop() > 850){
+        if ($(this).scrollTop() > 1000){
             headrOpacity.css('opacity','0.7');
         } 
     });
